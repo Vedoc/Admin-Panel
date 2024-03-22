5f9bc085f15e6146a50bb7bcb47bb33e8e33a06d
arnoldnekemiah <arnoldnek@gmail.com>
2024-03-22 15:08:20 +0300

    tweek2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  # config.assets.quiet = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
