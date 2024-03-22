aa79a6ca10a581377fcfbee5acd1ff1c1dd71bb7
arnoldnekemiah <arnoldnek@gmail.com>
2024-03-22 23:06:31 +0300

    reverted files that had assets method usage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,8 +44,12 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+
+  config.assets.debug = true
+
+
   # Suppress logger output for asset requests.
-  # config.assets.quiet = true
+  config.assets.quiet = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
