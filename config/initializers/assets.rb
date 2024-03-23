695bab3a269e3441f6cf382ce93bed7a49283e72
arnoldnekemiah <arnoldnek@gmail.com>
2024-03-23 13:38:49 +0300

    commented out config.assets since its deprecated

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,10 +3,10 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+# Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
@@ -14,9 +14,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # Handle the absence of the 'assets' method gracefully
-if Rails.application.config.respond_to?(:assets)
-  # If the 'assets' method is available, you can add your configuration here
-else
-  # If the 'assets' method is not available, you can print a warning or perform other actions
-  puts "Warning: Rails.application.config.assets is not available."
-end
+# if Rails.application.config.respond_to?(:assets)
+#   # If the 'assets' method is available, you can add your configuration here
+# else
+#   # If the 'assets' method is not available, you can print a warning or perform other actions
+#   puts "Warning: Rails.application.config.assets is not available."
+# end
