c7ca6cab7c379e1bf5bcdf126fdfd309fdae79a9
arnoldnekemiah <arnoldnek@gmail.com>
2024-03-23 08:30:47 +0300

    ignored NoMethod

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,16 +1,22 @@
-#app/config/initializers/assets.rb.
+# app/config/initializers/assets.rb
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-# Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
-# Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Handle the absence of the 'assets' method gracefully
+if Rails.application.config.respond_to?(:assets)
+  # If the 'assets' method is available, you can add your configuration here
+else
+  # If the 'assets' method is not available, you can print a warning or perform other actions
+  puts "Warning: Rails.application.config.assets is not available."
+end
